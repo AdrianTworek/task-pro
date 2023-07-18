807351a19a036420146798661263608f4878ac9b
Adrian Tworek <adrian.tworek.it@gmail.com>
2023-07-18 18:38:40 +0200

    feat: implement Google OAuth and email sign in

--- apps/web/src/app/api/auth/[...nextauth]/route.ts
+++ apps/web/src/app/api/auth/[...nextauth]/route.ts
@@ -1,19 +1,81 @@
-import type {NextAuthOptions} from 'next-auth'
-import NextAuth from "next-auth"
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import DiscordProvider from "next-auth/providers/discord"
-import { prisma } from "database"
+import type { NextAuthOptions } from 'next-auth';
+import NextAuth from 'next-auth';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { prisma } from 'database';
+import bcrypt from 'bcryptjs';
+import { z } from 'zod';
+
+import GoogleProvider from 'next-auth/providers/google';
+import DiscordProvider from 'next-auth/providers/discord';
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+const registerWithCredentialsSchema = z.object({
+  email: z.string(),
+  password: z.string(),
+});
 
 export const OPTIONS: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
+    GoogleProvider({
+      clientId: process.env['GOOGLE_CLIENT_ID']!,
+      clientSecret: process.env['GOOGLE_CLIENT_SECRET']!,
+    }),
     DiscordProvider({
-      clientId: process.env["DISCORD_CLIENT_ID"]!,
+      clientId: process.env['DISCORD_CLIENT_ID']!,
       clientSecret: process.env['DISCORD_CLIENT_SECRET']!,
     }),
+    CredentialsProvider({
+      name: 'email address',
+      credentials: {
+        email: {
+          label: 'Email',
+          type: 'email',
+        },
+        password: {
+          label: 'Password',
+          type: 'password',
+        },
+      },
+      async authorize(credentials) {
+        const { email, password } =
+          registerWithCredentialsSchema.parse(credentials);
+
+        const user = await prisma.user.findUnique({
+          where: { email: email },
+        });
+
+        // Register
+        if (!user) {
+          const hashedPassword = await bcrypt.hash(password, 12);
+
+          const newUser = await prisma.user.create({
+            data: {
+              email: email,
+              password: hashedPassword,
+            },
+          });
+
+          return newUser;
+        }
+
+        // Login
+        const isPasswordValid = await bcrypt.compare(
+          password,
+          user.password ?? ''
+        );
+
+        if (!isPasswordValid) return null;
+
+        return user;
+      },
+    }),
   ],
-  adapter: PrismaAdapter(prisma),
-  secret: process.env['NEXTAUTH_SECRET']
-}
+  session: {
+    strategy: 'jwt',
+  },
+  secret: process.env['NEXTAUTH_SECRET'],
+};
 
 const handler = NextAuth(OPTIONS);
 
