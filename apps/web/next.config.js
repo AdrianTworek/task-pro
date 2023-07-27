a2f31ed757bdcc3261d0709aaa7a030692c3d2c6
Kacper Hemperek <kacper.hemperek@o2.pl>
2023-07-27 23:25:30 +0200

    chore(web): allow all origins for images in

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ['ui', 'tailwindconfig'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
