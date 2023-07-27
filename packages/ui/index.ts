551295e643bdee59b8de704a8f3b61d7e221c966
Kacper Hemperek <kacper.hemperek@o2.pl>
2023-07-27 23:28:19 +0200

    chore(ui): add card card component

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -1,3 +1,4 @@
 export * from './src/components/ui/button';
 export * from './src/components/ui/dropdown-menu';
 export * from './src/components/ui/avatar';
+export * from './src/components/ui/card';
