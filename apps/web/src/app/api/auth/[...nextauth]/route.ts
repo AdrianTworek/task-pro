bda0bc41248af11cf05fc298326038ac96f41165
Kacper Hemperek <kacper.hemperek@o2.pl>
2023-07-28 20:06:49 +0200

    fix(web): add user id to token and session

--- apps/web/src/app/api/auth/[...nextauth]/route.ts
+++ apps/web/src/app/api/auth/[...nextauth]/route.ts
@@ -71,6 +71,22 @@ export const OPTIONS: NextAuthOptions = {
       },
     }),
   ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.role = user.role;
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (session?.user) {
+        session.user.role = token.role;
+        session.user.id = token.id;
+      }
+      return session;
+    },
+  },
   session: {
     strategy: 'jwt',
   },
