8906bb0202c123514a35dace60777014b44383ca
Kacper Hemperek <kacper.hemperek@o2.pl>
2023-07-29 12:24:15 +0200

    feat(web): get user's projects from backend

--- apps/web/src/app/dashboard/page.tsx
+++ apps/web/src/app/dashboard/page.tsx
@@ -1,74 +1,43 @@
 import { AddProjectCard } from '@/components/common/add-project/add-project-card';
 import { ProjectCard } from '@/components/common/project-card';
+import { GetProjectsResponse } from '@/server/project/project.services';
+import { headers } from 'next/headers';
 
 export default async function DashboardPage() {
-  const projects: Array<{
-    id: string;
-    name: string;
-    description: string;
-    createdAt: string;
-    users: Array<{ id: string; image: string }>;
-  }> = [
-    {
-      id: '1',
-      name: 'BuildEasy',
-      description:
-        'Sooo guys wyyy nyydaa, actually yes so imagine a scenario when GC',
-      createdAt: '2023-07-26T20:05:44.073Z',
-      users: [
-        {
-          id: '1',
-          image: 'https://avatars.githubusercontent.com/u/15075759?v=4',
-        },
-        {
-          id: '2',
-          image: 'https://avatars.githubusercontent.com/u/15075324?v=4',
-        },
-      ],
+  const res = await fetch(`${process.env['NEXTAUTH_URL']}api/projects`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      cookie: headers().get('cookie') ?? '',
     },
-    {
-      id: '2',
-      name: 'BuildEasy',
-      description:
-        'Sooo guys wyyy nyydaa, actually yes so imagine a scenario when GC',
-      createdAt: '2023-07-26T20:05:44.073Z',
-      users: [
-        {
-          id: '3',
-          image: 'https://avatars.githubusercontent.com/u/15075759?v=4',
-        },
-        {
-          id: '4',
-          image: 'https://avatars.githubusercontent.com/u/15075324?v=4',
-        },
-      ],
-    },
-    {
-      id: '3',
-      name: 'BuildEasy',
-      description:
-        'Sooo guys wyyy nyydaa, actually yes so imagine a scenario when GC',
-      createdAt: '2023-07-26T20:05:44.073Z',
-      users: [
-        {
-          id: '5',
-          image: 'https://avatars.githubusercontent.com/u/15075759?v=4',
-        },
-        {
-          id: '6',
-          image: 'https://avatars.githubusercontent.com/u/15075324?v=4',
-        },
-      ],
-    },
-  ];
+  });
+
+  if (!res.ok) {
+    const errorRes = await res.json();
+
+    if (errorRes.error) {
+      console.error(errorRes.error);
+    }
+
+    return <div>Something went wrong</div>;
+  }
+
+  const data = (await res.json()) as { projects: GetProjectsResponse };
 
   return (
-    <main className='container pt-12 flex flex-col'>
+    <main className='container py-12 flex flex-col'>
       <h1 className='text-4xl font-semibold mb-6'>Projects</h1>
       <div className='grid grid-cols-3 gap-8 xl:grid-cols-4'>
         <AddProjectCard />
-        {projects.map((project) => (
-          <ProjectCard {...project} key={'project__' + project.id} />
+        {data.projects.map((project) => (
+          <ProjectCard
+            createdAt={new Date(project.createdAt)}
+            description={project.description}
+            id={project.id}
+            name={project.name}
+            users={project.members}
+            key={'project__' + project.id}
+          />
         ))}
       </div>
     </main>
