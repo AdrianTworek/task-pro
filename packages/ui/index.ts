6d4ed704cd8a93bc740672ca18ff424a73669c0f
Kacper Hemperek <kacper.hemperek@o2.pl>
2023-08-03 22:49:39 +0200

    chore(ui): add table component

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -15,6 +15,8 @@ export * from './src/components/ui/toaster';
 export * from './src/components/ui/use-toast';
 export * from './src/components/ui/skeleton';
 export * from './src/components/ui/form';
+export * from './src/components/ui/table';
+
 export * from './src/components/submit-button';
 export * from './src/components/ui/sheet';
 export * from './src/components/ui/separator';
