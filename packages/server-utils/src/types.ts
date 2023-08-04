286bf922e1e9640dc03d47ca367250f3b91b1345
Kacper Hemperek <kacper.hemperek@o2.pl>
2023-08-04 22:45:46 +0200

    feat(app): add server utils as a package

---
+++ packages/server-utils/src/types.ts
@@ -0,0 +1,33 @@
+import { StatusCodes } from 'http-status-codes';
+
+export type CommonErrorResponse = {
+  error: {
+    code: StatusCodes;
+    message: string;
+  };
+};
+
+export type ValidationErrorResponse = {
+  error: {
+    code: StatusCodes;
+    message: string;
+    issues: {
+      field: string;
+      message: string;
+    }[];
+  };
+};
+
+export type ServerErrorResponse = CommonErrorResponse | ValidationErrorResponse;
+
+export function isValidationErrorResponse(
+  error: any
+): error is ValidationErrorResponse {
+  return error.error && error.error.issues;
+}
+
+export function isCommonErrorResponse(
+  error: any
+): error is CommonErrorResponse {
+  return error.error && !error.error.issues;
+}
