f361835340f2e7e25b870acc4c05e2383b744606
Kacper Hemperek <kacper.hemperek@o2.pl>
2023-08-21 21:15:05 +0200

    feat(admin): change unauthorized error message

--- apps/admin/src/server/project/project.fetcher.ts
+++ apps/admin/src/server/project/project.fetcher.ts
@@ -2,14 +2,14 @@ import { getProjects } from '@/server/project/project.services';
 import { getAppServerSession } from '@/utils/get-server-session';
 import { RoleEnum } from 'database';
 import { StatusCodes } from 'http-status-codes';
-import { CommonErrorResponse, ServerError } from 'server-utils';
+import { ServerError } from 'server-utils';
 
 export const fetchProjects = async () => {
   const session = await getAppServerSession();
 
   if (!session || session.user.role !== RoleEnum.ADMIN) {
     throw new ServerError({
-      message: 'Unauthorized',
+      message: 'Unauthorized - user is not an admin',
       code: StatusCodes.UNAUTHORIZED,
     });
   }
