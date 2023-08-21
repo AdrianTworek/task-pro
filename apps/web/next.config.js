eade514c86860fd638c8bf0dd73cd2e2e7a1786d
Kacper Hemperek <kacper.hemperek@o2.pl>
2023-08-21 20:46:20 +0200

    feat(admin): add server utils package to web

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ['ui', 'tailwindconfig'],
+  transpilePackages: ['ui', 'tailwindconfig', 'server-utils'],
   images: {
     remotePatterns: [
       {
