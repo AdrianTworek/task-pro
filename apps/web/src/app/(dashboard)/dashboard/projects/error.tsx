31f8afcd30fbc20da969b3abe86c0c86637d6e35
Kacper Hemperek <kacper.hemperek@o2.pl>
2023-08-28 15:16:33 +0200

    feat: add error page for projects list

---
+++ apps/web/src/app/(dashboard)/dashboard/projects/error.tsx
@@ -0,0 +1,31 @@
+'use client';
+import { AlertTriangleIcon } from 'lucide-react';
+import Link from 'next/link';
+import { Button } from 'ui';
+
+export default function ProjectError({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  return (
+    <div className="container flex flex-col justify-center items-center gap-4 mt-24">
+      <AlertTriangleIcon className="w-24 h-24" />
+      <div className="text-center">
+        <h2 className="scroll-m-20 text-2xl font-semibold tracking-tight mb-2">
+          Something went wrong!
+        </h2>
+        <p className="text-muted-foreground">{error.message}</p>
+      </div>
+      <Button variant="outline" onClick={reset}>
+        Try again
+      </Button>
+      <span>or</span>
+      <Link href="/dashboard/projects">
+        <Button>Go back to dashboard</Button>
+      </Link>
+    </div>
+  );
+}
